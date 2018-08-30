8fe983cd498bddfb478eb24b67c11143a04226d1
gyxoned <geyixiao831@gmail.com>
2018-08-30 17:12:56 +0800

    add cuhk03 new protocol

--- reid/datasets/__init__.py
+++ reid/datasets/__init__.py
@@ -3,6 +3,7 @@ import warnings
 
 from .cuhk01 import CUHK01
 from .cuhk03 import CUHK03
+from .cuhk03np import CUHK03NP
 from .dukemtmc import DukeMTMC
 from .market1501 import Market1501
 from .viper import VIPeR
@@ -12,6 +13,7 @@ __factory = {
     'viper': VIPeR,
     'cuhk01': CUHK01,
     'cuhk03': CUHK03,
+    'cuhk03np': CUHK03NP,
     'market1501': Market1501,
     'dukemtmc': DukeMTMC,
 }
--- scripts/train.sh
+++ scripts/train.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=0,1,2,3 \
-python examples/oim_loss.py --dataset cuhk03 --oim-scalar 30 \
-	--num-instances 7 --lr 0.009 --epochs 70 -b 112 --features 256 \
-	--logs-dir logs/oim-loss/cuhk03-resnet50/lr0.009
+CUDA_VISIBLE_DEVICES=4,5,6,7 \
+python examples/oim_loss.py --dataset market1501 --oim-scalar 30 \
+	--num-instances 7 --lr 0.0009 --epochs 70 -b 112 --features 256 \
+	--logs-dir logs/oim-loss/market1501-resnet50/lr0.0009val
