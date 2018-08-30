47d8982341ed99effe7282213139e9a46008ca99
gyxoned <geyixiao831@gmail.com>
2018-08-31 00:41:37 +0800

    update

--- scripts/train_soft.sh
+++ scripts/train_soft.sh
@@ -1,4 +1,4 @@
 CUDA_VISIBLE_DEVICES=4,5,6,7 \
-python examples/softmax_loss.py --dataset dukemtmc \
+python examples/softmax_loss.py --dataset cuhk03np \
 	--num-instances 7 --lr 0.01 --epochs 70 --ss 20 -b 112 --features 256 \
-	--logs-dir logs/softmax-loss/dukemtmc-resnet50/lr0.01-baseline
+	--logs-dir logs/softmax-loss/cuhk03np-resnet50/lr0.01-baseline
