ce1b4024c5c69cff6e04f3639b9e7e941d0bf7f8
yxgeee <geyixiao831@gmail.com>
2018-08-31 12:41:23 +0800

    update

--- scripts/train_soft.sh
+++ scripts/train_soft.sh
@@ -1,4 +1,4 @@
 CUDA_VISIBLE_DEVICES=0,1,2,3 \
-python examples/softmax_loss.py --dataset cuhk03np --combine-trainval \
+python examples/softmax_loss.py --dataset msmt17 \
 	--num-instances 7 --lr 0.01 --epochs 70 --ss 20 -b 112 --features 256 \
-	--logs-dir logs/softmax-loss/cuhk03np-resnet50/lr0.01-trainval
+	--logs-dir logs/softmax-loss/msmt17-resnet50/lr0.01
