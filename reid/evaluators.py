8801bb737abb09551516cf75288e4d2e7fb9cea0
gyxoned <geyixiao831@gmail.com>
2018-10-26 23:43:05 +0800

    add adaptive bn

--- reid/evaluators.py
+++ reid/evaluators.py
@@ -97,8 +97,8 @@ def adapt_source_bn(model, data_loader, print_freq=1):
 
     for idx, m in enumerate(model.modules()):
       if m.__class__.__name__.find('BatchNorm') != -1:
-        m.state_dict()['running_mean']=abn_param[idx].running_mean
-        m.state_dict()['running_var']=abn_param[idx].running_var
+        m.state_dict()['running_mean']=abn_param[idx].running_mean.cuda()
+        m.state_dict()['running_var']=abn_param[idx].running_var.cuda()
 
 def pairwise_distance(features, query=None, gallery=None, metric=None):
     if query is None and gallery is None:
@@ -205,36 +205,8 @@ def evaluate_all(distmat, query=None, gallery=None,
             return cmc_scores['market1501'][0], mAP
 
 
-    # Compute all kinds of CMC scores
-    # cmc_configs = {
-    #     'allshots': dict(separate_camera_set=False,
-    #                      single_gallery_shot=False,
-    #                      first_match_break=False),
-    #     'cuhk03': dict(separate_camera_set=True,
-    #                    single_gallery_shot=True,
-    #                    first_match_break=False),
-    #     'market1501': dict(separate_camera_set=False,
-    #                        single_gallery_shot=False,
-    #                        first_match_break=True)}
-    # cmc_scores = {name: cmc(distmat, query_ids, gallery_ids,
-    #                         query_cams, gallery_cams, **params)
-    #               for name, params in cmc_configs.items()}
-
-    # print('CMC Scores{:>12}{:>12}{:>12}'
-    #       .format('allshots', 'cuhk03', 'market1501'))
-    # for k in cmc_topk:
-    #     print('  top-{:<4}{:12.1%}{:12.1%}{:12.1%}'
-    #           .format(k, cmc_scores['allshots'][k - 1],
-    #                   cmc_scores['cuhk03'][k - 1],
-    #                   cmc_scores['market1501'][k - 1]))
-
-    # Use the allshots cmc top-1 score for validation criterion
-    # return mAP, cmc_scores['allshots'][0]
-    # return mAP
-
-
 class Evaluator(object):
-    def __init__(self, model, dataset=None):
+    def __init__(self, model, dataset='market1501'):
         super(Evaluator, self).__init__()
         self.model = model
         self.dataset = dataset
@@ -245,7 +217,7 @@ class Evaluator(object):
         return evaluate_all(distmat, query=query, gallery=gallery, dataset=self.dataset)
 
 class Evaluator_ABN(object):
-    def __init__(self, model, dataset=None):
+    def __init__(self, model, dataset='market1501'):
         super(Evaluator_ABN, self).__init__()
         self.model = model
         self.dataset = dataset
