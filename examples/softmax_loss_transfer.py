031933929acec9cd240254d5bdfb83ff381450bf
gyxoned <geyixiao831@gmail.com>
2018-11-04 21:44:00 +0800

    fix bug about evaluator

--- examples/softmax_loss_transfer.py
+++ examples/softmax_loss_transfer.py
@@ -127,13 +127,14 @@ def main(args):
         #metric.train(model, train_loader)
         #print("Validation:")
         # evaluator.evaluate(val_loader, dataset_ul.val, dataset_ul.val)
-        del model, evaluator
+        del model
+        # evaluator
         model_target = models.create(args.arch, pretrained=False, num_features=args.features, dropout=args.dropout, num_classes=num_classes, adaptation=False)
         model_target.load_state_dict(checkpoint['state_dict'])
         model_target = nn.DataParallel(model_target).cuda()
-        evaluator = Evaluator(model_target)
+        # evaluator = Evaluator(model_target)
         print("Test:")
-        evaluator.evaluate(test_loader_target, dataset_target.query, dataset_target.gallery)
+        evaluator.evaluate(test_loader_target, train_loader_target, dataset_target.query, dataset_target.gallery)
         return
 
     # Criterion
