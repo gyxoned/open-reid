663249d6408437e8c0d66bca854238dd7d93bbe4
yxgeee <geyixiao831@gmail.com>
2019-05-05 12:01:47 +0800

    update

--- scripts/train_distributed.sh
+++ scripts/train_distributed.sh
@@ -1,4 +1,4 @@
 CUDA_VISIBLE_DEVICES=0,1,2,3 \
 python examples/softmax_loss_distributed.py --dataset market1501 \
-	--num-instances 4 --lr 0.01 --epochs 50 --step-size 20 -b 64 --features 256 \
-	--logs-dir logs/softmax-loss/pt1.1-mp --dist-url 'tcp://10.1.72.208:23456'
+	--num-instances 4 --lr 0.01 --epochs 50 --step-size 20 -b 64 -j 16 --features 256 \
+	--logs-dir logs/softmax-loss/pt1.1-mp-w2 --dist-url 'tcp://10.1.72.207:23456' --world-size 2 --rank 0
