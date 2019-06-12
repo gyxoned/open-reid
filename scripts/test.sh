29789001e3749633e312305fd820e5957164932d
yxgeee <geyixiao831@gmail.com>
2019-06-12 16:34:46 +0800

    update

---
+++ scripts/test.sh
@@ -0,0 +1,9 @@
+GPU=$1
+ARCH=$2
+DATASET=$3
+MODEL=$4
+
+CUDA_VISIBLE_DEVICES=${GPU} \
+python examples/softmax_loss.py --dataset ${DATASET} -a ${ARCH} \
+        -b 256 -j 4 --features 256 --evaluate \
+    	--resume ${MODEL} 
--- scripts/train_distributed.sh
+++ scripts/train_distributed.sh
@@ -1,4 +1,4 @@
 CUDA_VISIBLE_DEVICES=0,1,2,3 \
-python examples/softmax_loss_distributed.py --dataset aic19 --height 256 --width 256 -a resnet_ibn50a \
-	--num-instances 0 --lr 0.01 --epochs 70 --step-size 20 -b 64 -j 16 --features 256 \
-	--logs-dir logs/softmax-loss/aic19/ibn50a-ins0-f256 --dist-url 'tcp://10.1.72.207:8845' --world-size 1 --rank 0
+python examples/softmax_loss_distributed.py --dataset market1501 --height 256 --width 128 -a resnet101 \
+	--num-instances 4 --lr 0.009 --epochs 70 --step-size 20 -b 64 -j 16 --features 512 \
+	--logs-dir logs/softmax-loss/market1501-resnet101/ins4-f512 --dist-url 'tcp://10.1.72.207:8845' --world-size 1 --rank 0
